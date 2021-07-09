52a400cbdb4f39fa45343a9c43fd19fb0aa5d746
Eden AI <67952486+ai-compare@users.noreply.github.com>
2021-07-09 14:58:16 +0200

    Update AI-Compare-OCR-python-requests.py

--- AI-Compare-OCR-python-requests.py
+++ AI-Compare-OCR-python-requests.py
@@ -1,20 +1,20 @@
 import requests
 
-# You can find the documentation here : https://www.ai-compare.com/v1/redoc/#operation/OCR
+# You can find the documentation here : https://api.edenai.run/v1/redoc/#section/How-it-works
 
 #Enter your API Token
-headers = {  'Authorization': 'Bearer your API Key'} #You can get your free API token here https://www.ai-compare.com/accounts/login/?next=/my_apis
+headers = {'Authorization': 'Bearer your API Key'}
 
 # Select API
-url = 'https://www.ai-compare.com/v1/pretrained/vision/ocr'
+url = 'https://api.edenai.run/v1/pretrained/vision/ocr'
 
 # Select providers, and text to detect
-payload = {'providers': '[\'ocr_space\', \'cognitives_service\', \'aws\', \'google_cloud\']','text_reference': '','language': 'French'}
+payload = {'providers': '[\'ocr_space\', \'microsoft\', \'aws\', \'google_cloud\']','text_reference': '','language': 'French'}
 
 # Select file to test
 files = [  ('files', open('Picture/example.jpg','rb'))]
 
-# Request to AI COMPARE
+# Request to Eden AI
 response = requests.request("POST", url, headers=headers, data = payload, files = files)
 
 # Print result
